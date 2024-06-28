80a5451d5bf4b50f3355c01a9479301cd0bd3b1c
Joseph Christopher Lepis <jlepis2010@gmail.com>
2024-06-27 18:13:27 -1000

    updated and moved hash to env var

--- src/index.js
+++ src/index.js
@@ -14,78 +14,54 @@ const BACKGROUND_IMAGE_IDS = [
 	// Add more image IDs as needed
   ];
 
-const AVATAR_IMAGE_ID = "7cc86372-5853-4028-e09a-44484ffe4600"
-const CLOUDFLARE_ACCOUNT_HASH = 'qce_E24q1nd01LtdzDOvJA'; 
+const AVATAR_IMAGE_ID = "468a29eb-b00d-4050-a1a4-40a8c1db8a00"
 
 export default {
 	async fetch(request, env, ctx) {
 	  try {
-		const jekyllUrl = env.JEKYLL_URL || "https://joseph-c-lepis.com"
-		console.log('Using Jekyll URL:', jekyllUrl)
+      const CLOUDFLARE_ACCOUNT_HASH = env.IMAGE_ACCOUNT_HASH;   
+      const jekyllUrl = env.JEKYLL_URL || "https://joseph-c-lepis.com"
   
-		const parsedUrl = new URL(request.url);
-		console.log("url: ", parsedUrl.pathname)
-  
-		// Function to get Cloudflare Images URL
-		const getCloudflareImageUrl = (imageId) => 
-		  `https://imagedelivery.net/${CLOUDFLARE_ACCOUNT_HASH}/${imageId}/public`;
-  
-		if (parsedUrl.pathname.match(/\.(jpg|jpeg|png|gif|webp|svg)$/i)) {
-		  console.log('Image request detected:', parsedUrl.pathname);
-  
-		  const requestedImageId = parsedUrl.pathname.split('/').pop();
-		  if (BACKGROUND_IMAGE_IDS.includes(requestedImageId)) {
-			let newImageId;
-			do {
-			  newImageId = BACKGROUND_IMAGE_IDS[Math.floor(Math.random() * BACKGROUND_IMAGE_IDS.length)];
-			} while (newImageId === requestedImageId);
-  
-			const imageUrl = getCloudflareImageUrl(newImageId);
-			console.log("looking for image: ", imageUrl);
-			const imageResponse = await fetch(imageUrl);
-  
-			if (imageResponse.ok) {
-			  return imageResponse;
-			} else {
-			  console.log("image not found: ", newImageId);
-			  return new Response('Image not found', { status: 404 });
-			}
-		  }
-		}
-  
-		// Fetch the resource from your Jekyll site
-		let response = await fetch(jekyllUrl + parsedUrl.pathname + parsedUrl.search);
-		const contentType = response.headers.get('Content-Type');
-  
-		if (contentType && contentType.includes('text/html')) {
-		  let html = await response.text();
-		  
-		  // Choose a random background image
-		  const randomImageId = BACKGROUND_IMAGE_IDS[Math.floor(Math.random() * BACKGROUND_IMAGE_IDS.length)];
-		  const randomImageUrl = getCloudflareImageUrl(randomImageId);
-		  
-		  html = html.replace(/<img src="splash\.png" id="fullscreen">/, '');
-		  
-		  // handle avatar image
-		  const avatarImageUrl = `https://imagedelivery.net/${CLOUDFLARE_ACCOUNT_HASH}/${AVATAR_IMAGE_ID}/avatar`
-		  html = html.replace(/<img src="headshot\.png" id="avatar">/, `<img src="${avatarImageUrl}" id="avatar">`);
-		  
-		  // Insert the style tag with the random background image
-		  const styleTag = `<style>body { background-image: url('${randomImageUrl}'); background-size: cover; }</style>`;
-  
-		  html = html.replace('</head>', `${styleTag}</head>`);
-		  
-		  // Return the modified HTML
-		  return new Response(html, {
-			headers: response.headers
-		  });
-		}
-		
-		// Return the original response for non-HTML requests
-		return response;
+      const parsedUrl = new URL(request.url);
+      console.log("url: ", parsedUrl.pathname)
+    
+      // Function to get Cloudflare Images URL
+      const getCloudflareImageUrl = (imageId) => 
+        `https://imagedelivery.net/${CLOUDFLARE_ACCOUNT_HASH}/${imageId}/public`;
+    
+      // Fetch the resource from your Jekyll site
+      let response = await fetch(jekyllUrl + parsedUrl.pathname + parsedUrl.search);
+      const contentType = response.headers.get('Content-Type');
+    
+      if (contentType && contentType.includes('text/html')) {
+        let html = await response.text();
+        
+        // Choose a random background image
+        const randomImageId = BACKGROUND_IMAGE_IDS[Math.floor(Math.random() * BACKGROUND_IMAGE_IDS.length)];
+        const randomImageUrl = getCloudflareImageUrl(randomImageId);
+        
+        html = html.replace(/<img src="splash\.png" id="fullscreen">/, '');
+        
+        // handle avatar image
+        const avatarImageUrl = `https://imagedelivery.net/${CLOUDFLARE_ACCOUNT_HASH}/${AVATAR_IMAGE_ID}/avatar`
+        html = html.replace(/<img src="headshot\.png" id="avatar">/, `<img src="${avatarImageUrl}" id="avatar">`);
+        
+        // Insert the style tag with the random background image
+        const styleTag = `<style>body { background-image: url('${randomImageUrl}'); background-size: cover; }</style>`;
+    
+        html = html.replace('</head>', `${styleTag}</head>`);
+        
+        // Return the modified HTML
+        return new Response(html, {
+        headers: response.headers
+        });
+      }
+      
+      // Return the original response for non-HTML requests
+      return response;
 	  } catch (error) {
-		console.error('Worker error:', error);
-		return new Response(`Worker error: ${error.message}`, { status: 500 });
+		  console.error('Worker error:', error);
+		  return new Response(`Worker error: ${error.message}`, { status: 500 });
 	  }
 	},
 };
